262160f10f7727556c5ea3c9cc9e98ebd740a792
mokunshao <mokunshao@foxmail.com>
2020-09-20 00:21:30 +0800

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,21 @@
 package com.company;
 
 public class Main {
+    public Integer n = 1;
+
+    public Main(Integer n) {
+        this.n = n;
+    }
+
+    public Integer getN() {
+        return n;
+    }
+
+    public void setN(Integer n) {
+        this.n = n;
+    }
 
     public static void main(String[] args) {
-        //	 write your code here
-        System.out.println("hello world");
+        System.out.println(new Main(13).getN());
     }
 }
